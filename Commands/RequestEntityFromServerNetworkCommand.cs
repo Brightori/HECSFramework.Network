29d6540f027d3e926d08330b0daef3c5879bcba8
Eugen Dubovik <e.dubovik@kefirgames.com>
2021-06-28 22:28:07 +0300

    add: request command

---
+++ Commands/RequestEntityFromServerNetworkCommand.cs
@@ -0,0 +1,16 @@
+﻿using HECSFramework.Core;
+using MessagePack;
+using System;
+
+namespace Commands
+{
+    [MessagePackObject]
+    public struct RequestEntityFromServerNetworkCommand : INetworkCommand
+    {
+        [Key(0)]
+        public Guid ClientID;
+
+        [Key(1)]
+        public Guid NeededEntity;
+    }
+}
