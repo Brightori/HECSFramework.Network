053d8ad9452b72e53242418aeb871fbf53cf3c03
Eugen Dubovik <e.dubovik@kefirgames.com>
2021-07-13 14:34:32 +0300

    add: interfaces fix: transform component

---
+++ Components/TransformComponent.cs
@@ -0,0 +1,16 @@
+﻿using HECSFramework.Network;
+
+namespace Components
+{
+    public partial class TransformComponent : INetworkComponent, IUnreliable
+    {
+        public int Version { get; set; }
+        public bool IsDirty { get; set; }
+
+        partial void InfoUpdated()
+        {
+            IsDirty = true;
+            Version++;
+        }
+    }
+}
