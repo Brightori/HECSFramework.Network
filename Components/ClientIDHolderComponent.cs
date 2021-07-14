b04054e92745f39a136f2ce94d129c903ab24ab7
Eugen Dubovik <e.dubovik@kefirgames.com>
2021-07-14 16:39:45 +0300

    upd: client id holder

--- Components/ClientIDHolderComponent.cs
+++ Components/ClientIDHolderComponent.cs
@@ -1,8 +1,10 @@
 ﻿using HECSFramework.Core;
+using System;
 
 namespace Components
 {
     public partial class ClientIDHolderComponent : BaseComponent
     {
+        public Guid ClientID { get; set; }
     }
 }
