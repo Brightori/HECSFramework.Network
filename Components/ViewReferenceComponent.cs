49ba6482ce5201e71a369e55954ee722fb72b719
Eugen Dubovik <e.dubovik@kefirgames.com>
2021-07-23 16:43:22 +0300

    add: view ref component

---
+++ Components/ViewReferenceComponent.cs
@@ -0,0 +1,11 @@
+﻿using HECSFramework.Core;
+using HECSFramework.Network;
+
+namespace Components
+{
+    public partial class ViewReferenceComponent : BaseComponent, INetworkComponent
+    {
+        public int Version { get; set; }
+        public bool IsDirty { get; set; }
+    }
+}
