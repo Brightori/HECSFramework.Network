8c933b45fc1a2911a1ee4d9bf556d36daa9c635b
vyacheslav <vyacheslav.b@playstrom.io>
2022-07-04 10:57:28 +0300

    restore ReplicatedNetworkEntityComponent

---
+++ Components/ReplicatedNetworkEntityComponent.cs
@@ -0,0 +1,15 @@
+﻿using HECSFramework.Core;
+using HECSFramework.Network;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Components
+{
+    [Serializable]
+    public class ReplicatedNetworkEntityComponent : BaseComponent, INotReplicable
+    {
+    }
+}
