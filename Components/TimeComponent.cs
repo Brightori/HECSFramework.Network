6d4883fb80451898bc998458ce78e99b00d65e95
vyacheslav <vyacheslav.b@playstrom.io>
2022-07-04 11:20:54 +0300

    - fix TimeComponent

--- Components/TimeComponent.cs
+++ Components/TimeComponent.cs
@@ -1,38 +1,10 @@
 ﻿using HECSFramework.Core;
-using HECSFramework.Server;
+using System;
 
 namespace Components
 {
-    public class TimeComponent : BaseComponent
+    public partial class TimeComponent : BaseComponent
     {
-        private Config config;
-        private long currentTick;
         public DateTime CurrentTime => DateTime.UtcNow;
-
-        public TimeComponent() { }
-        public TimeComponent(Config config)
-        {
-            this.config = config;
-            currentTick = Environment.TickCount64 / config.ServerTickMilliseconds;
-        }
-
-
-        /// <summary>Current tick</summary>
-        public long TickCount => currentTick;
-
-        /// <summary>Time elapsed since tick change</summary>
-        public long TimeAfterTick => Environment.TickCount64 - (currentTick * config.ServerTickMilliseconds);
-
-        /// <summary>Time to next tick</summary>
-        public long TimeUntilTick => config.ServerTickMilliseconds - TimeAfterTick;
-
-
-
-        public float DeltaTime => config.ServerTickMilliseconds / 1000f;
-
-
-        public void NextTick() => currentTick++;
-
-        
     }
 }
