c68f458421e871dce1b06f63c84eaf427e302413
vyacheslav <vyacheslav.b@playstrom.io>
2022-07-07 10:42:56 +0300

    add: TimeComponent

--- Components/TimeComponent.cs
+++ Components/TimeComponent.cs
@@ -3,8 +3,35 @@ using System;
 
 namespace Components
 {
-    public partial class TimeComponent : BaseComponent, IWorldSingleComponent
+    public class TimeComponent : BaseComponent, IWorldSingleComponent
     {
-        public DateTime CurrentTime => DateTime.UtcNow;
+        private long serverTickMilliseconds;
+        private long startSyncMillis, startLocalTime;
+        private uint currentTick;
+
+        public TimeComponent() { }
+        public void Start(long currentMillis, long serverTickMilliseconds)
+        {
+            startSyncMillis = currentMillis;
+            startLocalTime = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+            currentTick = (uint)(currentMillis / serverTickMilliseconds);
+            DeltaTime = serverTickMilliseconds / 1000f;
+        }
+
+        /// <summary>Current tick</summary>
+        public long TickCount => currentTick;
+        public long CurrentMillis => startSyncMillis + (DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() - startLocalTime);
+        public DateTime CurrentTime => DateTimeOffset.FromUnixTimeMilliseconds(CurrentMillis).DateTime;
+
+        /// <summary>Time elapsed since tick change</summary>
+        public long TimeAfterTick => CurrentMillis - (currentTick * serverTickMilliseconds);
+
+        /// <summary>Time to next tick</summary>
+        public long TimeUntilTick => serverTickMilliseconds - TimeAfterTick;
+
+        public float DeltaTime { get; private set; }
+       
+
+        public void NextTick() => currentTick++;
     }
 }
